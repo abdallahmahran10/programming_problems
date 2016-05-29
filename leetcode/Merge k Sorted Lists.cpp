50604eab038a51647a60ef601833656448eae7be
Abdallah Mahran <eng.abdallah.mahran@gmail.com>
2016-05-29 20:02:20 +0200

    Adding Merge k Sorted Lists

---
+++ leetcode/Merge k Sorted Lists.cpp
@@ -0,0 +1,149 @@
+#include <iostream>
+//#include "merge_two_sorted_arrays.h"
+#include <vector>
+using namespace std;
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+ListNode* mergeTwoLists(ListNode** list1, ListNode* list2)
+{
+    ListNode* slider1 = *list1;
+    ListNode* slider2 = list2;
+    if(list2 == NULL)
+        return *list1;
+    if(*list1 == NULL)
+        return list2;
+    ListNode* prevSlider;
+    ListNode* tmpSlider;
+    prevSlider = NULL;
+    while(slider2!= NULL)
+    {
+        while(slider1 != NULL && slider1->val <= slider2->val)
+        {
+            prevSlider = slider1;
+            slider1 = slider1->next;
+        }
+        if(prevSlider == NULL)
+            *list1 = slider2;
+        else
+            prevSlider->next = slider2;
+        tmpSlider = prevSlider = slider2;
+        slider2 = slider2->next;
+        tmpSlider->next = slider1;
+    }
+    return *list1;
+}
+//
+ListNode* mergeListsRec(vector<ListNode*>& lists, int pos)
+{
+    if(pos == lists.size())
+        return NULL;
+    ListNode* list1 = lists[pos];
+    ListNode* list2 = mergeListsRec(lists, pos+1);
+    return mergeTwoLists(&list1, list2);
+}
+//
+ListNode* mergeKLists(vector<ListNode*>& lists)
+{
+    int k =lists.size();
+    if(k == 1)
+        return lists[0];
+    return mergeListsRec(lists, 0);
+}
+
+ListNode* testCase0()
+{
+    vector<ListNode*> lists;
+    // list 1
+    ListNode* head1=new ListNode(1);
+    head1->next=new ListNode(3);
+    // list 2
+    ListNode* head2=new ListNode(2);
+    head2->next=new ListNode(4);
+    //
+    lists.push_back(head1);
+    lists.push_back(head2);
+    return mergeKLists(lists);
+}
+ListNode* testCase1()
+{
+    vector<ListNode*> lists;
+    // list 1
+    ListNode* head1=new ListNode(0);
+    head1->next=new ListNode(1);
+    //
+    lists.push_back(head1);
+    return mergeKLists(lists);
+}
+ListNode* testCase2()
+{
+    vector<ListNode*> lists;
+    // list 1
+    ListNode* head1=new ListNode(1);
+    // list 2
+    ListNode* head2=new ListNode(0);
+    //
+    lists.push_back(head1);
+    lists.push_back(head2);
+    return mergeKLists(lists);
+}
+ListNode* testCase3()
+{
+    vector<ListNode*> lists;
+    // list 1
+    ListNode* head1=new ListNode(1);
+    // list 2
+    ListNode* head2=new ListNode(0);
+    //
+    lists.push_back(NULL);
+    lists.push_back(head1);
+    return mergeKLists(lists);
+}
+ListNode* testCase4()
+{
+    //Input: [[1,2,2],[1,1,2]]
+    //Output: [1,1,1,2,2,2]
+    vector<ListNode*> lists;
+    // list 1
+    ListNode* head1=new ListNode(1);
+    head1->next=new ListNode(2);
+    head1->next->next=new ListNode(2);
+    // list 2
+    ListNode* head2=new ListNode(1);
+    head2->next=new ListNode(1);
+    head2->next->next=new ListNode(2);
+    //
+    lists.push_back(head1);
+    lists.push_back(head2);
+    return mergeKLists(lists);
+}
+ListNode* testCase5()
+{
+    //Input: [[1,2,3],[4,5,6,7]]
+    //Output: [1,2,3,4,5,6,7]
+    vector<ListNode*> lists;
+    // list 1
+    ListNode* head1=new ListNode(1);
+    head1->next=new ListNode(2);
+    head1->next->next=new ListNode(3);
+    // list 2
+    ListNode* head2=new ListNode(4);
+    head2->next=new ListNode(5);
+    head2->next->next=new ListNode(6);
+    head2->next->next->next=new ListNode(7);
+    //
+    lists.push_back(head1);
+    lists.push_back(head2);
+    return mergeKLists(lists);
+}
+
+int main()
+{
+    ListNode *result = testCase5();
+    return 0;
+}
+
--- playground/playground/playground.cpp
+++ playground/playground/playground.cpp
@@ -3,58 +3,95 @@
 #include "stdafx.h"
 #include "common.h" // my defined macros
 
-
-//11340 - Newspaper
-int uva11340()
+/////////////////////uva problems template /////////////////////
+//12356 - Army Buddies
+int uva12356()
 {
 #ifdef LOCAL
 	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
-        
+	freopen("output.txt", "w", stdout);        
 #endif
 	// Your code is here //////////////////////////
-	map<char , int> charsValues;
-	int v, linesCount;
-	char ch;
-	IN(tc);
-	string line;
-	double total;
-	while(tc--)
-	{
-		total = 0;
-		IN(n);
-		getline(cin, line);
-		F(n)
-		{
-			ch = getchar() ;
-			SCAN_INT(v);
-			charsValues[ch] = v;
-			getline(cin, line);
-		}
-		SCAN_INT(linesCount);
-		getline(cin, line);		
-		F(linesCount)
-		{
-			getline(cin, line);
-			F2(line.length())
-				total += charsValues[line[j]]; // map return default value(0) in case key does not exist
-			
-		}
-		printf("%.2f$\n",total/100);
-		charsValues.clear();
-	}
+	
 	///////////////////////////////////////////////
 #ifdef LOCAL
 	fclose (stdout);
 #endif
 	return 0;
 }
+/////////////////////////////// LEETCODE ////////////////////////////////
+// Definitions
+struct RandomListNode {
+	int label;
+	RandomListNode *next, *random;
+	RandomListNode(int x) : label(x), next(NULL), random(NULL) {}
+};
+//
+RandomListNode *deepCopyNode(RandomListNode *node, map<int, RandomListNode *>& copiedValues )
+{
+	RandomListNode *retNode;
+	if(copiedValues.count(node->label) != 0)
+		return copiedValues[node->label];
+	//
+	retNode = new RandomListNode(node->label);
+	copiedValues[node->label] = retNode;
+	//
+	if(node->next != NULL)
+		retNode->next = deepCopyNode(node->next, copiedValues);
+	if(node->random != NULL)
+		retNode->random = deepCopyNode(node->random, copiedValues);
+	return retNode;
+}
+// solution
+RandomListNode *copyRandomList(RandomListNode *head) 
+{
+	if( head == NULL)
+		return NULL;
+	map<int , RandomListNode *> copiedValues;
+	RandomListNode *retList;
+	retList = deepCopyNode(head, copiedValues);
+	return retList;
+}
+// test cases
+void testCase0()
+{
+	RandomListNode *head = NULL;
+	head = new RandomListNode(1);
+
+	head->next = new RandomListNode(2);
+	/*random of node 1*/ head->random = head->next/*node 2*/;
+	/*random of node 2*/ head->next->random = head /*node 1*/;
+	RandomListNode *result = copyRandomList(head);
+}
+//
+void testCase1()
+{
+	RandomListNode *head = NULL;
+	head = new RandomListNode(1);
+
+	head->next = new RandomListNode(2);
+	head->next->next = new RandomListNode(3);
+	head->next->next->next  = new RandomListNode(4);
+	head->next->next->next->next = new RandomListNode(5);
+	/*random of node 1*/ head->random = head->next->next /*node 3*/;
+	/*random of node 2*/ head->next->random = head /*node 1*/;
+	/*random of node 3*/ head->next->next->random  = head->next->next->next->next /*node 5*/;
+	/*random of node 4*/ head->next->next->next->random =  head->next->next /*node 3*/;
+	/*random of node 5*/ head->next->next->next->next->random = head->next /*node 2*/;
+	RandomListNode *result = copyRandomList(head);
+}
+//
+void testCase2()
+{
+	RandomListNode *head = NULL;
+	head = new RandomListNode(1);
+	RandomListNode *result = copyRandomList(head);
+}
+//
 //////////////////////////////////////////////////////////////////////
 int _tmain(int argc, _TCHAR* argv[])
 {
-	uva11340();
-	//cout<<txt[0];
-	//
+	testCase2();
 	system("pause");
 }
 
