e87c2f4e70a73a20dbb34897d77aac49d45c233f
Abdallah Mahran <eng.abdallah.mahran@gmail.com>
2017-01-11 23:25:40 +0100

    traverse Tree create tree from preorder and inorder arrays

--- msc/BST.h
+++ msc/BST.h
@@ -43,7 +43,49 @@ public:
 		BST<T> bst(fromSortedVector(dataVec));
 		return bst;
 	}
+	//
+	
+	static BST CreateBSTFromPreorderAndInorder(vector<T>& preorder, vector<T>& inorder)
+	{
+		BST<T> bst(fromPreorderAndInorder(preorder, inorder));
+		return bst;
+	}
 private:
+	static TreeNode<T> *fromPreorderAndInorder(vector<T>& preorder, vector<T>& inorder)
+	{
+		if (preorder.empty() || inorder.size()!=preorder.size())
+			return NULL;
+		return buildTree(preorder, inorder, 0, preorder.size()-1, 0, inorder.size()-1);
+	}
+	//
+	static TreeNode<T> *buildTree(vector<T>& preorder, vector<T>& inorder, int ps, int pe, int is, int ie)
+	{
+		if(pe<ps)
+			return NULL;
+		TreeNode<T> *node = new TreeNode<T>(preorder[ps]);
+		vector<T>::iterator inOrderStartItr= next(inorder.begin(), is);
+		vector<T>::iterator inOrderEndItr= next(inorder.begin(), ie);
+		vector<T>::iterator it = find(inOrderStartItr, inOrderEndItr, preorder[ps]);
+		if(it == inorder.end())
+			return NULL;
+		if(pe - ps == 1)
+		{
+		    if(preorder[ps] == inorder[is])
+			    node->rightChild = new TreeNode<T>(preorder[ps + 1]);
+			 else
+			    node->leftChild = new TreeNode<T>(preorder[ps + 1]);
+			return node;
+		}
+		else if(pe - ps == 0)
+			return node;
+		//
+		int idx = it - inOrderStartItr;
+		node->leftChild = buildTree(preorder, inorder, ps+1 ,  ps + idx, is,  is +  idx-1);
+		node->rightChild = buildTree(preorder, inorder,ps + idx+1,  pe, is + idx+1, ie);
+		return node;
+	}
+
+	//
 	static TreeNode<T> *fromSortedVector(std::vector<T> &dataVec)
 	{
 		if (dataVec.empty())
@@ -68,13 +110,22 @@ private:
 private:
 	TreeNode<T> *mRoot;
 	// print preorder traversal
-	void preOrder(TreeNode<T>* node)
+	void preOrderTraversal(TreeNode<T>* node)
 	{
-		if (node == NULL)
+		if(node == NULL)
 			return;
-		std::cout << node->data << " ";
-		preOrder(node->leftChild);
-		preOrder(node->rightChild);
+		std::cout<<node->data<< " "; 
+		preOrderTraversal(node->leftChild);
+		preOrderTraversal(node->rightChild);
+	}
+	//
+	void inOrderTraversal(TreeNode<T>* node)
+	{
+		if(node == NULL)
+			return;
+		inOrderTraversal(node->leftChild);
+		std::cout<<node->data<< " "; 
+		inOrderTraversal(node->rightChild);
 	}
 /*********** Public Members **********/
 public:
@@ -131,7 +182,14 @@ public:
 	//
 	void preOrderPrint()
 	{
-		preOrder(mRoot);
+		preOrderTraversal(mRoot);
+		std::cout<<std::endl;
+	}
+	//
+	void inOrderPrint()
+	{
+		inOrderTraversal(mRoot);
+		std::cout<<std::endl;
 	}
 };
 
--- playground/playground/playground.cpp
+++ playground/playground/playground.cpp
@@ -2,7 +2,7 @@
 //
 #include "stdafx.h"
 #include "common.h" // my defined macros
-
+#include "BST.h"
 //#include "MaximumAbsoluteDifference.h"
 /////////////////////uva problems template /////////////////////
 //12356 - Army Buddies
@@ -35,102 +35,22 @@ void Main()
 #endif
 }
 
-int partition(vector<int> &nums, int s, int e)
-{
-	int pivotIdx = e;
-	for(int i=e-1; i>=s; i--)
-	{
-		if(nums[i]< nums[pivotIdx])
-		{
-			int j = i;
-			if(abs(i-pivotIdx) > 1)
-			{
-				while(j<pivotIdx-1)
-					swap(nums[j], nums[j++]);
-				
-			} 
-			swap(nums[j], nums[pivotIdx]);
-			pivotIdx = j;
-		}
-	}
-	return pivotIdx;
-}
-
-void quickSort(vector<int> &nums, int s, int e)
-{
-	if(s<e)
-	{
-		int pivotIdx = partition(nums, s, e);
-		quickSort(nums, s, pivotIdx-1);
-		quickSort(nums, pivotIdx+1, e);
-	}
-}
-void mergeInPlace(vector<int> &nums, int s, int m, int e)
-{
-}
-
-void merge(vector<int> &nums, int s, int m, int e)
-{
-	int size1 = m-s + 1;
-	int size2 = e-m;
-	int i,j;
-	vector<int> L(size1);
-	vector<int> R(size2);
-
-	for(i=0; i<size1; i++)
-		L[i] = nums[i + s];
-	for(j=0; j<size2; j++)
-		R[j] = nums[j+m+1];
-	int idx=s;
-	i=0,j=0;
-	while(i<size1 && j<size2)
-	{
-		if(L[i]<R[j])
-		{
-			nums[idx++] = L[i];
-			i++;
-		} else 
-		{
-			nums[idx++] = R[j];
-			j++;
-		}
-	}
-
-	while(i<size1)
-		nums[idx++] = L[i++];
-
-	while(j<size2)
-		nums[idx++] = R[j++];
-}
-
-void mergeSort(vector<int> &nums, int s, int e)
-{
-	if(s<e)
-	{
-		int mid = s+(e-s)/2;
-		mergeSort(nums, s, mid);
-		mergeSort(nums, mid+1, e);
-		merge(nums, s, mid, e);
-	}
-}
-
-vector<int>& sort(vector<int>& nums)
-{
-	quickSort(nums, 0, nums.size()-1);
-    return nums;
-}
 
 void testCase()
 {
 	int a[] = {3,9,5,2,6,7,1,8};
 	vector<int> nums(a, a + (sizeof a / sizeof a[0]));
-	printVector( sort(nums));
-	int a1[] = { 0,1,0,1,0,1,0,1};
-	vector<int> nums1(a1, a1 + (sizeof a1 / sizeof a1[0]));
-	printVector( sort(nums1));
-	int a2[] = {-1,-2,1,13,0,0,-23};
-	vector<int> nums2(a2, a2 + (sizeof a2 / sizeof a2[0]));
-	printVector( sort(nums2));
+	int apre[] ={ 5,2, 1, 3, 7, 6, 8, 9 };
+	int ain[] ={  1, 2, 3, 5, 6, 7, 8, 9 };
+	vector<int> pre(apre, apre + (sizeof apre / sizeof apre[0]));
+	vector<int> in(ain, ain + (sizeof ain / sizeof ain[0])); 
+
+
+	BST<int> bst1 = BST<int>::CreateBSTFromPreorderAndInorder(pre, in);
+	//BST<int> bst = BST<int>::CreateBST(nums, false);
+	printVector(nums);
+	bst1.preOrderPrint();
+	bst1.inOrderPrint();
 	//cout<<solve("aaabccddd")<<endl;
 	//cout<<solve("baab")<<endl;
 	//cout<<solve("bb")<<endl;
