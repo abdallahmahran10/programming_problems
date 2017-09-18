f37df1715198e4076b5057e78a7cf0f3c70bc486
Abdallah Mahran <eng.abdallah.mahran@gmail.com>
2017-09-18 21:22:31 +0200

    Update and rename TwoSum.cpp to two-sum.h

--- leetcode/two-sum.h
+++ leetcode/two-sum.h
@@ -3,11 +3,36 @@
    Ref: https://leetcode.com/problems/two-sum/
 */
 
-#include "common.h"
+#include "../common.h"
 
-
-typedef unordered_map<int, int>::const_iterator cit;
+// generic solution 
+vector<pair<int, int>> findPairs(vector<int> v, int s)
+{
+	vector<pair<int, int>> pairs;
+	unordered_map<int, int> hm;
+	for(int i=0; i < v.size(); i++)
+		hm[v[i]] = i;
+	for(int i=0; i < v.size(); i++)
+	{
+		unordered_map<int, int>::iterator it = hm.find(s-v[i]);
+		if(it != hm.end() && it->second != i)
+			pairs.push_back(make_pair(i, it->second));
+	}
+	return pairs;
+}
 vector<int> twoSum(vector<int>& nums, int target) {
+	vector<pair<int, int>> pairs = findPairs(nums, target);
+	vector<int> res;
+	if(!pairs.empty())
+	{
+		res.push_back(pairs[0].first);
+		res.push_back(pairs[0].second);
+	}
+	return res;
+}
+// basic solution
+typedef unordered_map<int, int>::const_iterator cit;
+vector<int> twoSum1(vector<int>& nums, int target) {
 	vector<int> result; 
 	unordered_map<int, int> hash;
 	for (int i = 0; i < nums.size(); i++)
