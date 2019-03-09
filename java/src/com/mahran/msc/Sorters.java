0587e0f2ca01f1057b523346754e6a8f27fa1367
Abdallah Mahran <ammahra@amazon.com>
2019-03-08 22:47:57 -0800

    Add selectionSort and bubbleSort (Java)

---
+++ java/src/com/mahran/msc/Sorters.java
@@ -0,0 +1,33 @@
+package com.mahran.msc;
+
+public class Sorters {
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIdx = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIdx]) {
+                    minIdx = j;
+                }
+            }
+            if (minIdx != i) {
+                swap(arr, i, minIdx);
+            }
+        }
+    }
+
+    public static void bubbleSort(int[] arr) {
+        for (int i = arr.length - 1; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr, j, j + 1);
+                }
+            }
+        }
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+}
